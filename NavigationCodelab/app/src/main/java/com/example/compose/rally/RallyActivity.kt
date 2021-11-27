662d94d7be67728c877aa7890fe880cb7fe40b34
Richard B Winters <rik@mmod.co>
2021-11-27 14:53:17 -0500

    Completed Navigation in Jetpack Compose Codelab
    * There is an issue with running tests if targetSdk is 31
      * Ignoring, because I don't feel like downgrading the project to targetSDK <31, and the next codelab is the testing codelab.
      * Ultimately, some dependency somewhere is not properly setting exported=true, and so manifest merge is throwing an error when trying to run tests.
    * App builds and installs and launches and runs just fine.

--- NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -22,13 +22,24 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.*
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.compose.rally.data.UserData
+import com.example.compose.rally.ui.accounts.AccountsBody
+import com.example.compose.rally.ui.accounts.SingleAccountBody
+import com.example.compose.rally.ui.bills.BillsBody
 import com.example.compose.rally.ui.components.RallyTabRow
+import com.example.compose.rally.ui.overview.OverviewBody
 import com.example.compose.rally.ui.theme.RallyTheme
 
 /**
@@ -48,23 +59,184 @@ class RallyActivity : ComponentActivity() {
 fun RallyApp() {
     RallyTheme {
         val allScreens = RallyScreen.values().toList()
-        var currentScreen by rememberSaveable { mutableStateOf(RallyScreen.Overview) }
+        val navController = rememberNavController()
+        //var currentScreen by rememberSaveable { mutableStateOf(RallyScreen.Overview) }
+        val backstackEntry = navController.currentBackStackEntryAsState()
+        val currentScreen = RallyScreen.fromRoute(
+            backstackEntry.value?.destination?.route
+        )
         Scaffold(
             topBar = {
                 RallyTabRow(
                     allScreens = allScreens,
-                    onTabSelected = { screen -> currentScreen = screen },
+                    //onTabSelected = { screen -> currentScreen = screen },
+                    onTabSelected = { screen -> navController.navigate( screen.name ) },
                     currentScreen = currentScreen
                 )
             }
         ) { innerPadding ->
-            Box(Modifier.padding(innerPadding)) {
+            // TODO: 1-1 Use this:
+            RallyNavHost(
+                navController = navController,
+                modifier = Modifier.padding( innerPadding )
+            )
+
+            /* TODO: 1-2 To remove this:
+            val accountsName = RallyScreen.Accounts.name
+            NavHost(
+                navController = navController,
+                startDestination = RallyScreen.Overview.name,
+                modifier = Modifier.padding( innerPadding )
+            ) {
+                // Each of these top level composable are NavGraph extension methods
+                // that allow us to specify a route
+                composable( RallyScreen.Overview.name ) {
+                    //Text( text = RallyScreen.Overview.name )
+                    OverviewBody(
+                        onClickSeeAllAccounts = { navController.navigate( RallyScreen.Accounts.name ) },
+                        onClickSeeAllBills = { navController.navigate( RallyScreen.Bills.name ) },
+                        onAccountClick = {
+                            navController.navigate( "${RallyScreen.Accounts.name}/$it" )
+                        }
+                    )
+                }
+                composable( RallyScreen.Accounts.name ) {
+                    //Text( text = RallyScreen.Accounts.name )
+                    AccountsBody(
+                        accounts = UserData.accounts
+                    ) {
+                        // onAccountClick is the last parameter, and so may be provided in a
+                        // trailing lambda
+                        navController.navigate( "${RallyScreen.Accounts.name}/$it" )
+                    }
+                }
+                composable( RallyScreen.Bills.name ) {
+                    // Text( text = RallyScreen.Bills.name )
+                    BillsBody(
+                        bills = UserData.bills
+                    )
+                }
+                // Here we specify a route that accepts arguments
+                //
+                // You have to store RallyScreen.Accounts.name
+                // in a variable - or an exception is thrown
+                // when you try to navigate to a single account
+                // with an error that the path doesn't exist!
+                composable(
+                    route = "$accountsName/{name}",
+                    arguments = listOf(
+                        navArgument( "name" ) {
+                            // Make argument type safe
+                            type = NavType.StringType
+                        }
+                    ),
+                    deepLinks = listOf(
+                        navDeepLink {
+                            uriPattern = "rally://$accountsName/{name}"
+                        }
+                    )
+                ) {
+                    entry -> // Look up "name" in NavBackStackEntry's arguments
+                    // And how to use the arguments
+                    val accountName = entry.arguments?.getString( "name" )
+                    // Find first name match in UserData
+                    val account = UserData.getAccount( accountName )
+                    // Pass account to SingleAccountBOdy (instead of AccountsBody)
+                    SingleAccountBody( account = account )
+                }
+            }*/
+
+            /*Box(Modifier.padding(innerPadding)) {
                 currentScreen.content(
                     onScreenChange = { screen ->
                         currentScreen = RallyScreen.valueOf(screen)
                     }
                 )
-            }
+            }*/
         }
     }
+}
+
+@Composable
+fun RallyNavHost (
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = RallyScreen.Overview.name,
+        modifier = modifier
+    ) {
+        // NOTE: RuntimeException Warning:
+        // We have to store the Accounts screen name (from RallyScreen enum) in a variable
+        // because we cannot access data members from a route string at runtime - an error is
+        // thrown.
+        val accountsName = RallyScreen.Accounts.name
+        composable( RallyScreen.Overview.name ) {
+            OverviewBody(
+                onClickSeeAllAccounts = { navController.navigate( RallyScreen.Accounts.name ) },
+                onClickSeeAllBills = { navController.navigate( RallyScreen.Bills.name ) },
+                onAccountClick = {
+                    //navController.navigate( "${RallyScreen.Accounts.name}/$it" )
+                    // Let's provide a helper method instead, for our extracted NavHost:
+                    navigateToSingleAccount(
+                        navController = navController,
+                        accountName = it
+                    )
+                }
+            )
+        }
+        composable( RallyScreen.Accounts.name ) {
+            AccountsBody(
+                accounts = UserData.accounts
+            ) {
+                // onAccountClick is the last parameter, so it can be provided in a trailing lambda
+                // navController.navigate( "${RallyScreen.Accounts.name}/$it" )
+                // Let's again use the helper method for our extracted NavHost
+                navigateToSingleAccount(
+                    navController = navController,
+                    accountName = it
+                )
+            }
+        }
+        composable( RallyScreen.Bills.name ) {
+            BillsBody(
+                bills = UserData.bills
+            )
+        }
+        // Here we specify a route that accepts arguments
+        composable(
+            //route = "$RallyScreen.Accounts.name/{name}", // See RuntimeException above
+            route = "$accountsName/{name}",
+            arguments = listOf(
+                navArgument( "name" ) {
+                    // Make argument type safe
+                    type = NavType.StringType
+                }
+            ),  // Test deeplinks after installing latest application version with:
+                // ~/Android/Sdk/platform-tools/adb shell am start \
+                // -d "rally://accounts/Checking" \
+                // -a android.intent.action.VIEW
+            deepLinks = listOf( navDeepLink {
+                    //uriPattern = "$RallyScreen.Accounts.name/{name}", // See RuntimeException above
+                    uriPattern = "rally://$accountsName/{name}"
+                }
+            )
+        ) {
+            entry -> // Look up "name" in NavBackStackEntry's arguments
+            // And how to use the arguments
+            val accountName = entry.arguments?.getString( "name" )
+            // Find first name match in UserData
+            val account = UserData.getAccount( accountName )
+            // Pass account to SingleAccountBOdy (instead of AccountsBody)
+            SingleAccountBody( account = account )
+        }
+    }
+}
+
+private fun navigateToSingleAccount(
+    navController: NavHostController,
+    accountName: String
+) {
+    navController.navigate( "${RallyScreen.Accounts.name}/$accountName" )
 }
