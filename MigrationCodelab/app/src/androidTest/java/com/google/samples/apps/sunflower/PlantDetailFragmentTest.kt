030b2dcc218ce538422083bf25945ddfacee65b6
Richard B Winters <rik@mmod.co>
2022-01-04 17:00:57 -0500

    Completed Migrations in Compose Codelab

--- MigrationCodelab/app/src/androidTest/java/com/google/samples/apps/sunflower/PlantDetailFragmentTest.kt
+++ MigrationCodelab/app/src/androidTest/java/com/google/samples/apps/sunflower/PlantDetailFragmentTest.kt
@@ -20,6 +20,9 @@ import android.accessibilityservice.AccessibilityService
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
 import androidx.navigation.Navigation.findNavController
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
@@ -50,7 +53,8 @@ class PlantDetailFragmentTest {
 
     @Rule
     @JvmField
-    val activityTestRule = ActivityScenarioRule(GardenActivity::class.java)
+    //val activityTestRule = ActivityScenarioRule(GardenActivity::class.java)
+    val composeTestRule = createAndroidComposeRule<GardenActivity>() // Testing compose in Android
 
     // Note that keeping these references is only safe if the activity is not recreated.
     private lateinit var activity: ComponentActivity
@@ -59,7 +63,8 @@ class PlantDetailFragmentTest {
     fun jumpToPlantDetailFragment() {
         populateDatabase()
 
-        activityTestRule.scenario.onActivity { gardenActivity ->
+        //activityTestRule.scenario.onActivity { gardenActivity ->
+        composeTestRule.activityRule.scenario.onActivity { gardenActivity ->
             activity = gardenActivity
 
             val bundle = Bundle().apply { putString("plantId", "malus-pumila") }
@@ -69,8 +74,12 @@ class PlantDetailFragmentTest {
 
     @Test
     fun testPlantName() {
-        onView(ViewMatchers.withText("Apple"))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        // We migrated this part of the UI to compose
+        // onView(ViewMatchers.withText("Apple"))
+        //    .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        //
+        // So we need to use compose assertions instead:
+        composeTestRule.onNodeWithText( "Apple" ).assertIsDisplayed()
     }
 
     @Test
